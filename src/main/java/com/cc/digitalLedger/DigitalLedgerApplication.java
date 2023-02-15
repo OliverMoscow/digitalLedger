7107618b7cdba898e698e8519e3a8544dc716b01
OliverMoscow <56978722+OliverMoscow@users.noreply.github.com>
2023-02-15 14:10:59 -0700

    test

--- src/main/java/com/cc/digitalLedger/DigitalLedgerApplication.java
+++ src/main/java/com/cc/digitalLedger/DigitalLedgerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DigitalLedgerApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DigitalLedgerApplication.class, args);
 	}
